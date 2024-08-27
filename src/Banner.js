2331f78b108b22136be184e975e57c8a6b66d5fa
RohitNegi59 <rohitnegi5910@gmail.com>
2024-08-27 10:01:52 +0530

    delete some  images

--- src/Banner.js
+++ src/Banner.js
@@ -3,7 +3,9 @@ import './index.css';
 export default function Banner() {
   return (
     <div className='Banner'>
-      <div className='main_img'></div>
+      <div className='main_img'>
+        
+      </div>
       <div className='main_desc'>
       Masters in Computer Application graduate with a strong foundation in programming languages and web development 
 technologies. Proficient in HTML, CSS, JavaScript ,React Js, Python, C++,Mongo Db, SQL,. Adept at using Figma for 
