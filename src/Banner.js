e12e2415ccd239116b26fe16277d08f9fa744abe
RohitNegi59 <rohitnegi5910@gmail.com>
2024-08-27 17:18:53 +0530

    font change

--- src/Banner.js
+++ src/Banner.js
@@ -8,10 +8,11 @@ export default function Banner() {
       </div>
       <div className='main_desc inner-div'>
         <h1>ROHIT NEGI</h1>
-        Masters in Computer Application graduate with a strong foundation in programming languages and web development
-        technologies. Proficient in HTML, CSS, JavaScript ,React Js, Python, C++,Mongo Db, SQL,. Adept at using Figma for
-        design. Eager to contribute to software development projects and gain practical experience.
-
+        <h2 className='banner_text'><span className='blue'>Masters in Computer Application </span>graduate with a strong foundation in programming languages and <span className='red'> web development </span> 
+         technologies. Proficient in <span className='blue'>HTML, CSS, JavaScript ,React Js, Python, C++,Mongo Db, SQL,. Adept at using Figma</span> for
+        design. Eager to contribute to web and <span className='red'> software development</span>  projects and gain practical experience.
+</h2>
+    
       </div>
     </div>
   )
--- src/Mainbox.js
+++ src/Mainbox.js
@@ -6,37 +6,37 @@ function Mainbox() {
             <div className='mainbox'>
                 <div className="mycard Education ">
                     <div className='innercard inner-div'>
-                    <h1>Education</h1>
+                        <h1>Education</h1>
                         <ul>
-                            <li><h2>MCA (Master of Computer Application) from Lovely Professional University
+                            <li><h2 className='blue'>MCA (Master of Computer Application) from Lovely Professional University
                                 Phagwara-(2022-2024). </h2></li>
-                            <li><h2>B.A (Bachelor of Art) from Chaudhary Charan University
+                            <li><h2 className='red'>B.A (Bachelor of Art) from Chaudhary Charan University
                                 Saharanpur-(2019-2022)</h2></li>
-                            <li><h2>Intermediate(12th class) from St Thomas Academy Saharanpur-(2018)</h2>
+                            <li><h2 className='blue'>Intermediate(12th class) from St Thomas Academy Saharanpur-(2018)</h2>
                             </li>
-                            <li><h2>High School(10th class) from St Thomas Academy Saharanpur-(2016)</h2>
+                            <li><h2 className='red'>High School(10th class) from St Thomas Academy Saharanpur-(2016)</h2>
                             </li>
                         </ul></div>
                     <div className='innercard2 inner-div'>
-                    <img className="edu_img" src='/images/School.png'></img>
+                        <img className="edu_img" src='/images/School.png'></img>
                     </div>
                 </div>
 
 
                 <div className="mycard skill  ">
                     <div className='innercard2 inner-div'>
-                    <img className="edu_img" src='/images/Skill.png'></img>
+                        <img className="edu_img" src='/images/Skill.png'></img>
                     </div>
                     <div className='innercard inner-div'>
-                    <h1>Skill</h1>
+                        <h1>Skill</h1>
                         <ul>
-                            <li><h2> Programming Languages: Python ,C++,C,Java  </h2></li>
-                            <li><h2></h2></li>
-                            <li><h2>Web Development: HTML , CSS , JavaScript , SQL ,React js </h2>
+                            <li><h2 className='red'> Programming Languages: Python ,C++,C,Java  </h2></li>
+                            <li><h2 className='blue'>Website Builder : Shopify ,WordPress ,Wix</h2></li>
+                            <li><h2 className='red'>Web Development: HTML , CSS , JavaScript , SQL ,React js </h2>
                             </li>
-                            <li><h2>MS Office : Power Bi ,MS Word , MS Excel , MS Powerpoint </h2>
+                            <li><h2 className='blue'>MS Office : Power Bi ,MS Word , MS Excel , MS Powerpoint </h2>
                             </li>
-                            <li><h2>Design Tools: Figma ,Canva, Photoshop</h2>
+                            <li><h2 className='red'>Design Tools: Figma ,Canva, Photoshop</h2>
                             </li>
                         </ul></div>
                     <div>
@@ -48,12 +48,13 @@ function Mainbox() {
                 <div className="mycard project  ">
 
                     <div className='innercard inner-div'>
-                    <h1>Project</h1>
+                        <h1>Project</h1>
                         <ul>
-                            <li><h2> Playbook - web application for children to enhance creativity and learning. Built with React.js
+                            <li><h2 className='blue'> Playbook - web application for children to enhance creativity and learning. Built with React.js
                                 for an engaging user interface  </h2></li>
-                            <li><h2></h2></li>
-                            <li><h2>Online Food Ordering Portal- "The Himalayan Bites " restaurant app using MERN to simplify the food ordering
+                            <li><h2 className='red'> Personal Portfolio Website - Personal portfolio web app using MERN to show my skills ,knowledge learning . project
+                                will also help Recruiter and HR they can see my work directly from site .</h2></li>
+                            <li><h2 className='blue'>Online Food Ordering Portal- "The Himalayan Bites " restaurant app using MERN to simplify the food ordering
                                 experience. Users can browse restaurants, view menus, and place orders
                                 directly through the app. This project focused on creating a user-friendly interface for efficient restaurant
                                 discovery
@@ -63,7 +64,7 @@ function Mainbox() {
                         </ul>
                     </div>
                     <div className='innercard2 inner-div'>
-                    <img className="edu_img" src='/images/project.png'></img>
+                        <img className="edu_img" src='/images/project.png'></img>
                     </div>
 
                 </div>
@@ -75,17 +76,17 @@ function Mainbox() {
 
                     </div>
                     <div className='innercard inner-div'>
-                    <h1>Certifications</h1>
-                    <ul>
-                                <li><h2>Fundamental of HTML from Great Learning Aug 26 2024
-                                </h2></li>
-                                <li><h2>Fundamentals of Python Programmingat from Ybi Foundation in Oct 31 2023.</h2></li>
-                                <li><h2>Python101 for Data Science by IBM in Oct 06 2023</h2>
-                                </li>
-                                <li><h2>Prompt Engineering For Every by IBM skill Network Oct 31 2023
-                                </h2>
-                                </li>
-                            </ul></div>
+                        <h1>Certifications</h1>
+                        <ul>
+                            <li><h2 className='red'>Fundamental of HTML from Great Learning Aug 26 2024
+                            </h2></li>
+                            <li><h2 className='blue'>Fundamentals of Python Programmingat from Ybi Foundation in Oct 31 2023.</h2></li>
+                            <li><h2 className='red'>Python101 for Data Science by IBM in Oct 06 2023</h2>
+                            </li>
+                            <li><h2 className='blue'>Prompt Engineering For Every by IBM skill Network Oct 31 2023
+                            </h2>
+                            </li>
+                        </ul></div>
                     <div>
 
                     </div>
